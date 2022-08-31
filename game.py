c16009e6f006cbb2ec60da3bd347e0d993e808d9
Amadeus8182 <amadeus8182@gmail.com>
2022-08-31 11:07:28 -0500

    name change and added score variable

--- game.py
+++ game.py
@@ -4,7 +4,8 @@ class Game:
     def __init__(self):
         self.bd = Board(1)
         self.possibleMoves = ['Left', 'Right', 'Down', 'Drop', 'RotateR', 'RotateL', 'Rotate180']
-
+        self.score = 0
+        
     def play(self):
         loss = False
         while not loss:

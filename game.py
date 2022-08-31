123241952c1b5c827e6260dce355c432519f1345
Amadeus8182 <amadeus8182@gmail.com>
2022-08-31 10:58:21 -0500

    changed game.py to a class, added move handler in board class

--- game.py
+++ game.py
@@ -1,16 +1,11 @@
 from Board import Board
-import os
-import time
 
-def main():
-    bd = Board(1)
-    for i in range(5):
-        bd.update()
-        time.sleep(0.5)
-        os.system('clear')
-        
+class Game:
+    def __init__(self):
+        self.bd = Board(1)
+        self.possibleMoves = ['Left', 'Right', 'Down', 'Drop', 'RotateR', 'RotateL', 'Rotate180']
 
-
-if __name__ == '__main__':
-    os.system('clear')
-    main()
+    def play(self):
+        loss = False
+        while not loss:
+            self.bd.update()
